bcf730eedbfe56ca84f6686d94633c193801ab95
tylerpercy <tjpercy332@gmail.com>
2024-12-03 10:42:37 -0500

    refrctoring get_data

--- main.py
+++ main.py
@@ -1,59 +1,36 @@
 import discord
 import os
-import requests
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from discord.ext import commands
-
-class match:
-
-    home_team_id: str
-    home_spread: str
-    away_team_id: str
-    away_spread: str
-    total: float
-
-    def __init__(self, homeTeamId, homeSpread, awayTeamId, awaySpread, total):
-        self.home_team_id = homeTeamId
-        self.home_spread = homeSpread
-        self.away_team_id = awayTeamId
-        self.away_spread = awaySpread
-        self.total = total
+from match import build_match_list
+from stats import get_sport_data
 
 intents=discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-def format_data(betting_lines, team_ids):
-
-    matches = []
-
-    grouped_lines = [betting_lines[i:i+4][:-1] for i in range(0, len(betting_lines), 4)]
-    grouped_teams = [team_ids[i:i+2] for i in range(0, len(team_ids), 2)]
-
-    for lines, teams in zip(grouped_lines, grouped_teams):
-        #print(f"{teams[0]}: {lines[0]}")
-        #print(f"{teams[1]}: {lines[2]}")
-        #print(f"Total: {lines[1]}")
-        #print("")
-        matches.append(match(teams[1], lines[2], teams[0], lines[0], lines[1]))
-
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-
 @bot.command()
-async def get_nfl(ctx):
-    url = 'https://sportsbook.draftkings.com/leagues/football/nfl'
-    response = requests.get(url)
-    response.raise_for_status()
+async def get_data(ctx, sport_id: str = None, sport_team: str = None, *args):
+    sport_urls = {
+        "nfl": os.getenv('URL_NFL'),
+        "nba": os.getenv('URL_NBA'),
+        "nhl": os.getenv('URL_NHL'),
+    }
+
+    url = sport_urls.get(sport_id.lower())
+    if not url:
+        await ctx.send(f"Invalid sport_id: {sport_id}. Please specify 'nfl', 'nba', or 'nhl'.")
+        return
+
+    betting_lines, team_ids = get_sport_data(url)
+    matches = build_match_list(betting_lines, team_ids)
+
+    msg = "\n".join(str(match) for match in matches)
+    await ctx.send(msg)
     
-    soup = BeautifulSoup(response.text, 'html.parser')
-    betting_lines = [value.text for value in soup.find_all('span', class_='sportsbook-outcome-cell__line')]
-    team_ids = [value.text for value in soup.find_all('div', class_='event-cell__name-text')]
-
-    format_data(betting_lines, team_ids)
-
 load_dotenv()
 bot.run(os.getenv('BOT_TOKEN'))
